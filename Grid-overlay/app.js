24fb872c829335e8f95c2c515a25cf66f2ddbef4
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-07-28 08:07:18 +0300

    set up todoapp project

--- Grid-overlay/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".toggle").click(function (e) {
-        if ($(".show").css("display") === "none") {
-            $(".show").slideDown();
-        } else {
-            $(".show").slideUp();
-        }
-    });
-});
